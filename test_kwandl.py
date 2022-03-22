ce81f3e0d1b8fafae328ecda8f8a13d78e997022
E. G. Patrick Bos <egpbos@gmail.com>
2022-03-22 17:07:24 +0100

    added test for unexpanded kwargs corner case

--- test_kwandl.py
+++ test_kwandl.py
@@ -31,3 +31,27 @@ def test_replace_kwargs_in_calls():
 def test_replace_kwargs_in_calls_throw_type_error_on_unexpected_kwarg():
     with pytest.raises(TypeError, match=r"function_1_and_2\(\) got an unexpected keyword argument 'nonexistent_kwarg'"):
         function_1_and_2_kwandled(nonexistent_kwarg=1)
+
+
+def function_with_dict_parameter(just_a_dict=None):
+    return just_a_dict
+
+
+@kwandl.replace_kwargs_in_calls
+def function_with_unexpanded_kwargs(**kwargs):
+    result1 = function1(**kwargs)
+    result2 = function2(**kwargs)
+    result3 = function_with_dict_parameter(kwargs)
+    return result1, result2, result3
+
+
+def test_replace_kwargs_in_calls_with_unexpanded_kwargs():
+    """
+    Test the corner case of kwargs being passed just as a dictionary.
+    
+    In this case, kwargs in function_with_dict_parameter(kwargs) becomes a non-keyword
+    argument in the AST (i.e. it will be in the args attribute of the ast.Call, instead of
+    the keywords attribute), which means it won't trigger the forwarding decorator at all.
+    """
+    result = function_with_unexpanded_kwargs(kwarg1=1, kwarg2=2)
+    assert(result == ({'kwarg1': 1}, {'kwarg2': 2}, {'kwarg1': 1, 'kwarg2': 2}))
